4aa47af69575b4c979bef14fede5bd043680d133
timmonfette1 <monfette.timothy@gmail.com>
2017-06-02 01:48:45 -0400

    Initial Commit; KH 1.5 Trophies

---
+++ README.md
@@ -0,0 +1,15 @@
+Kingdom Hearts 1.5+2.5 Trophy Tracker
+===================
+
+`<Insert link to website here>`
+
+A web based tracker for Platinum Trophy RTA Speedruns for Kingdom Hearts 1.5+2.5 for the PS4.  Currently, only Kingdom Hearts 1 Final Mix is supported here, but support for Kingdom Hearts 2 Final Mix trophies is coming soon.<br />
+
+All trophies start in Black and White meaning they haven't been obtained yet.<br />
+Hovering your mouse over a trophy will give a tool-tip with the trophy name.<br />
+Once you obtain a trophy, you click on it and it will become colored.<br />
+This will allow for more clear tracking of trophies instead of using splits that can vary in length from run to run because of RNG.
+
+NOTE
+-------
+Please note that refreshing the page will RESET ALL TROPHIES TO BLACK AND WHITE.
---
+++ js/trophytracker.js
@@ -0,0 +1,36 @@
+// Build path to images
+// Needs to get current directory on the fly since it can
+//   change during deployment.
+var loc = window.location.pathname;
+var dir = loc.substring(0, loc.lastIndexOf('/'));
+var defaultPath = "file://".concat(dir);
+defaultPath = defaultPath.concat("/images/");
+
+function changeImage(trophyName, imageSrc) {
+ 
+  var path = defaultPath;
+
+  // Game = res[0], Trophy = res[1]
+  var res = trophyName.split("-");
+
+  // Check what game the trophy is for
+  if (res[0] == "kh1") { 
+    path = path.concat("kh1/"); 
+  } else {
+    path = path.concat("kh2/");
+  }
+
+  // Check if the image should be changed to color or Black/White
+  if (~imageSrc.indexOf("-bw")) {
+    path = path.concat("color/");
+    path = path.concat(res[1]);
+    path = path.concat(".png");
+  } else {
+    path = path.concat("bw/");
+    path = path.concat(res[1]);
+    path = path.concat("-bw.png");
+  }
+
+  // Update image
+  document.getElementById(trophyName).src = path;
+}
